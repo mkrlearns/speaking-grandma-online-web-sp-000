c0f26c5ec9f4c4e48c2f2563cc890d6d36700409
ryan m <contact@ryanmeek.com>
2019-02-26 20:57:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,6 +14,9 @@
 
 def speak_to_grandma(string)
   if string === "I LOVE YOU GRANDMA!"
-    return 
-  if string === string.upcase
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif string === string.upcase
     return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+end
